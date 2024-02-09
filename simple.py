e40067453436c37bbdd378a7a6fc8ff436b28bef
Thomas <thomas.hameon99@gmail.com>
2024-02-09 16:22:02 +0100

    First commit

---
+++ simple.py
@@ -0,0 +1,14 @@
+# Comment
+
+print("Hello world!")
+
+def function1():
+    print('in function 1')
+
+def function2(tekst):
+    print('ik print nu de tekst: ' + tekst)
+
+function1()
+function2('Dit is een tekst')
+
+getal1 = 12
